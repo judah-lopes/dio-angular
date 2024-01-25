645c235d3c27e6c0dc4962a3c90840644293eda3
judah-lopes <lopes.judah@gmail.com>
2024-01-25 17:21:10 -0300

    doc: Estrutura Condicional Com If e Else
    Commit com:
    - tópico "Estrutura Condicional Com If e Else"

--- JAVASCRIPT/Primeiros Passos com Javascript/aula 02 - estruturas condicionais/script.js
+++ JAVASCRIPT/Primeiros Passos com Javascript/aula 02 - estruturas condicionais/script.js
@@ -1,6 +1,10 @@
 
-const numero = 11
+const numero = 10
 
 const numeroPar = (numero % 2) === 0
 
-console.log(numeroPar)
+if (numeroPar) {
+    console.log(`Par`)
+} else {
+    console.log(`Impar`)
+}
