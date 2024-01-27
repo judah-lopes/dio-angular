170fdaa01b3a767177ea8d99a1c92d0779c10d68
judah-lopes <lopes.judah@gmail.com>
2024-01-26 21:48:18 -0300

    doc: Exemplo prático com funções
    Commit com:
    - tópicos "Exemplo prático com funções"

--- JAVASCRIPT/2 - Conhecendo Funções Com JavaScript/aula 01 - O Poder das Funções na Qualidade do Seu Código/script.js
+++ JAVASCRIPT/2 - Conhecendo Funções Com JavaScript/aula 01 - O Poder das Funções na Qualidade do Seu Código/script.js
@@ -13,15 +13,45 @@
 
 //! Como Organizar as Funções
 
-function calcularJuros() {
+// function calcularJuros() {
     
+// }
+
+// function main() {
+//     console.log(`Programa principal`)
+//     calcularJuros()
+// }
+
+// main()
+
+/* -------------------------------------------------------------------------------------------- */
+
+//! Exemplo prático com funções
+
+function calcularImc(peso,altura) {
+    return peso / Math.pow(altura,2)
 }
 
-function main() {
-    console.log(`Programa principal`)
-    calcularJuros()
+function classificarImc(imc) {
+    if (imc < 18.5) {
+        return(`O IMC do paciente é ${imc.toFixed(1)}. O paciente está abaixo do peso adequado.`)
+    } else if (imc < 25) {
+        return(`O IMC do paciente é ${imc.toFixed(1)}. O paciente está dentro do peso adequado.`)
+    } else if (imc < 30) {
+        return(`O IMC do paciente é ${imc.toFixed(1)}. O paciente está acima do peso adequado.`)
+    } else if (imc < 40) {
+        return(`O IMC do paciente é ${imc.toFixed(1)}. O paciente está obeso.`)
+    } else {
+        return(`O IMC do paciente é ${imc.toFixed(1)}. O paciente está com obesidade mórbida.`)
+    }
+}
+
+function main(){
+    let peso = 140
+    let altura = 1.86
+
+    const imc = calcularImc(peso, altura)
+    console.log(classificarImc(imc))
 }
 
 main()
-
-
