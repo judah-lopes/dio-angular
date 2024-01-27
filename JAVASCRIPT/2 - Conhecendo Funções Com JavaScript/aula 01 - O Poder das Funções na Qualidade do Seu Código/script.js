938cab5bfd33f90fe9d3e92ae64b655922662cac
judah-lopes <lopes.judah@gmail.com>
2024-01-26 21:32:34 -0300

    doc: Como Organizar as Funções
    Commit com:
    -tópico "Como Organizar as Funções"

--- JAVASCRIPT/2 - Conhecendo Funções Com JavaScript/aula 01 - O Poder das Funções na Qualidade do Seu Código/script.js
+++ JAVASCRIPT/2 - Conhecendo Funções Com JavaScript/aula 01 - O Poder das Funções na Qualidade do Seu Código/script.js
@@ -1,10 +1,27 @@
+//! Conceito Básico Sobre Funções
 
+// function incrementarJuros(valor, percentualJuros) {
+//     const valorDeAcrecimo = (percentualJuros / 100) * valor
+//     return valor + valorDeAcrecimo
+// }
 
-function incrementarJuros(valor, percentualJuros) {
-    const valorDeAcrecimo = (percentualJuros / 100) * valor
-    return valor + valorDeAcrecimo
+// console.log(incrementarJuros(100,10))
+// console.log(incrementarJuros(100,20))
+// console.log(incrementarJuros(100,15))
+
+/* -------------------------------------------------------------------------------------------- */
+
+//! Como Organizar as Funções
+
+function calcularJuros() {
+    
 }
 
-console.log(incrementarJuros(100,10))
-console.log(incrementarJuros(100,20))
-console.log(incrementarJuros(100,15))
+function main() {
+    console.log(`Programa principal`)
+    calcularJuros()
+}
+
+main()
+
+
