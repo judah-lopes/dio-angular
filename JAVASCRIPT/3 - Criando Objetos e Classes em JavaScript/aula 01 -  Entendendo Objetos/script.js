565809aa9b4fc46150e9a01b5bba5efced2415a6
judah-lopes <lopes.judah@gmail.com>
2024-01-30 17:54:57 -0300

    doc: Editando informações de um objeto
    Commit com:
    - tópico "Editando informações de um objeto"

--- JAVASCRIPT/3 - Criando Objetos e Classes em JavaScript/aula 01 -  Entendendo Objetos/script.js
+++ JAVASCRIPT/3 - Criando Objetos e Classes em JavaScript/aula 01 -  Entendendo Objetos/script.js
@@ -1,10 +1,24 @@
 //! Estrutura básica de um objeto
 
-const pedro = {
-    nome: "Lucas Pereira Sampaio",
-    idade: 25
-}
+// const pedro = {
+//     nome: "Lucas Pereira Sampaio",
+//     idade: 25
+// }
 
-console.log(pedro.nome)
-console.log(pedro.idade)
-console.log(pedro)
+// console.log(pedro.nome)
+// console.log(pedro.idade)
+// console.log(pedro)
+
+    
+//! Editando informações de um objeto
+
+    const pedro = {
+        nome: "Lucas Pereira Sampaio",
+        idade: 25
+    }
+
+    pedro.genero = "Masculino" /* Criando uma nova informação */
+
+    delete pedro.nome /* Deletando uma informação */    
+
+    console.log(pedro)
