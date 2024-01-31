236b2318218c21fdf42d106291bdb7e0d3a3e6fc
judah-lopes <lopes.judah@gmail.com>
2024-01-30 22:13:57 -0300

    doc: Criando Instâncias Com Constructor
    Commit com:
    - tópico "Criando Instâncias Com Constructor"

--- JAVASCRIPT/3 - Criando Objetos e Classes em JavaScript/aula 02 - Entendendo Classes/script.js
+++ JAVASCRIPT/3 - Criando Objetos e Classes em JavaScript/aula 02 - Entendendo Classes/script.js
@@ -1,22 +1,24 @@
 class User {
     nome
     idade
+    anoDeNascimento
+
+    constructor(nome,idade) {
+        this.nome = nome
+        this.idade = idade
+        this.anoDeNascimento = 2024 - idade /* Cria "meta"-atributo novo sem precisar dá-lo como param */
+    }
 
     apresentar() {
         console.log(`Olá, meu nome é ${this.nome} e eu tenho ${this.idade} anos.`)
     }
 }
 
-const maria = new User();
-maria.nome = "Maria Clara"
-maria.idade = 21
-
-const pedro = new User();
-pedro.nome = "Pedro Judah"
-pedro.idade = 20
+const maria = new User("Maria Clara", 21);
+const pedro = new User("Pedro Judah", 20);
 
 console.log(maria)
 console.log(pedro)
 
-pedro.apresentar()
-maria.apresentar()
+// pedro.apresentar()
+// maria.apresentar()
