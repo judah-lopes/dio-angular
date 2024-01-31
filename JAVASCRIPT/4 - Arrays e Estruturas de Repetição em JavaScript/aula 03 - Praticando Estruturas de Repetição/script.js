7d72e9720897c8a5ded17c803582985c6eb25f7a
judah-lopes <lopes.judah@gmail.com>
2024-01-31 20:00:34 -0300

    doc: ✅Exercício 2
    Commit com:
    - tópico "Exercício 2"

--- JAVASCRIPT/4 - Arrays e Estruturas de Repetição em JavaScript/aula 03 - Praticando Estruturas de Repetição/script.js
+++ JAVASCRIPT/4 - Arrays e Estruturas de Repetição em JavaScript/aula 03 - Praticando Estruturas de Repetição/script.js
@@ -5,3 +5,14 @@ const numero = 2
 for (let i = 0; i <= 10; i++) {
     console.log(i * numero)
 }
+
+//! 2 - Crie um programa que percorra um array e só imprima números pares
+
+const numeros = [1,2,3,4,5,6,7,8,9,10]
+
+for (let i = 0; i < numeros.length; i++) {
+    const contagem = numeros[i];
+    if (contagem % 2 === 0) {
+        console.log(contagem)
+    }
+}
