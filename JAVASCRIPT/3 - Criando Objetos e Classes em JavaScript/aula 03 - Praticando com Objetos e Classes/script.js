d60134d85dced85e5b7612732be8509fd9e4c4da
judah-lopes <lopes.judah@gmail.com>
2024-01-31 17:18:23 -0300

    doc: 📝 Exercício 2
    Commit com:
    tópico: "Criando a Instância"

--- JAVASCRIPT/3 - Criando Objetos e Classes em JavaScript/aula 03 - Praticando com Objetos e Classes/script.js
+++ JAVASCRIPT/3 - Criando Objetos e Classes em JavaScript/aula 03 - Praticando com Objetos e Classes/script.js
@@ -26,3 +26,34 @@ console.log(clio.calcularGastoDePercurso(73,5.5))
 
 const palio = new Carro('Fiat','cinza',9)
 console.log(clio.calcularGastoDePercurso(100,5.5))
+
+//! Criando a Classe Pessoa
+//  2 - Crie uma classe para representar pessoas.
+//  Para cada pessoas teremos os atributos nome, peso e altura.
+//  As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = PESO / (ALTURA * ALTURA))
+//  Instancie uma pessoa chamada José que tenha 70kg de peso e 
+//  1,75 de altura e peça para o José dizer o valor do seu IMC
+
+class Pessoa {
+    nome 
+    peso 
+    altura
+
+    constructor(nome, peso, altura) {
+        this.nome = nome
+        this.peso = peso
+        this.altura = altura
+    }
+
+    calcularImc(){
+        const imc = this.peso / (this.altura ** 2)       
+        return `Meu nome é ${this.nome}, minha altura é ${this.altura}, meu peso é ${this.peso} e sendo assim, o valor do meu IMC é ${imc.toFixed(1)}`
+    }
+}
+
+const pedro = new Pessoa('Pedro', 96, 1.86);
+console.log(pedro.calcularImc())
+
+
+
+
