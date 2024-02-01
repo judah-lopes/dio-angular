5adf3ace03118a726b72c6b739ba1768a33353f5
judah-lopes <lopes.judah@gmail.com>
2024-02-01 16:33:28 -0300

    doc: Criando a Função gets e print
    Commit com:
    - tópico "Criando a Função gets e print"

---
+++ JAVASCRIPT/5 - Importação e Exportação com JavaScript/aula 01 - Desvendando as Funções gets e print/funcoes-auxiliares.js
@@ -0,0 +1,10 @@
+function gets() {
+    return 10
+}
+function print(texto){
+    console.log(texto)
+}
+
+// module.exports.gets = gets
+
+module.exports = {gets, print}
---
+++ JAVASCRIPT/5 - Importação e Exportação com JavaScript/aula 01 - Desvendando as Funções gets e print/main.js
@@ -0,0 +1,4 @@
+const funcoes = require('./funcoes-auxiliares')
+
+console.log(funcoes)
+console.log(funcoes.gets())
