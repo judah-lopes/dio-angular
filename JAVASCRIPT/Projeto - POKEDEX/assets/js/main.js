2ca53ce30fe7e191d196e4a2b80bf728fe621940
judah-lopes <lopes.judah@gmail.com>
2024-02-10 23:28:47 -0300

    doc: (Parte 1) Manipulando o resultado da requisição através de uma Promise
    Commit com:
    - primeira explicação do tópico
    "Manipulando o resultado da requisição através de uma Promise" da aula "Integrando com a PokeAPI"

--- JAVASCRIPT/Projeto - POKEDEX/assets/js/main.js
+++ JAVASCRIPT/Projeto - POKEDEX/assets/js/main.js
@@ -2,7 +2,18 @@ const offset = 0
 const limit = '10'
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-//fetch = requisitar / then = async
-fetch(url).then(function (response) {
-    console.log(response)
-})
+//fetch = requisitar 
+fetch(url)
+    .then(function (response) {   //then = quando certo, response
+        response
+            .json()
+            .then(function (responseBody) {      //response.json() = converte a response (body) em um tipo "any", json
+                console.log(responseBody)
+            })
+    })
+    .catch(function (error) {     //catch = quando errado, error
+        console.error(error);
+    })
+    .finally(function () {        // finally = quando operação termina, não importa certo, ou errado
+        console.log(`Requisição concluída!`)
+    })
