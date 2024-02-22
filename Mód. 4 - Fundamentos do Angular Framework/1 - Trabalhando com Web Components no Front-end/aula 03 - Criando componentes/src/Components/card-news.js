17f22a8c30c32a639a98401c099a81ebdc0e9a3d
judah-lopes <lopes.judah@gmail.com>
2024-02-22 17:38:42 -0300

    TESTE PESSOAL
    Commit com:
    - tentativa de decorar o card só com a estilização

--- Mód. 4 - Fundamentos do Angular Framework/1 - Trabalhando com Web Components no Front-end/aula 03 - Criando componentes/src/Components/card-news.js
+++ Mód. 4 - Fundamentos do Angular Framework/1 - Trabalhando com Web Components no Front-end/aula 03 - Criando componentes/src/Components/card-news.js
@@ -18,8 +18,51 @@ class CardNews extends HTMLElement {
                 </div>
             </div>
         `
+        const style = document.createElement("style")
+        style.textContent = `
+        *{
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+        
+        .card-news{
+            width: 80%;
+            
+            display: flex;
+            justify-content: space-between;
+        
+            box-shadow: 14px 10px 16px -7px rgba(0,0,0,0.55);
+            -webkit-box-shadow: 14px 10px 16px -7px rgba(0,0,0,0.55);
+            -moz-box-shadow: 14px 10px 16px -7px rgba(0,0,0,0.55);
+        }
+        
+        .card-left{
+            padding: 10px;
+        
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+        }
+        
+        .card-left span{
+            font-weight: 400;
+        }
+        
+        .card-left h1{
+            margin-top: 15px;
+            font-size: 25px;
+        }
+        
+        .card-left p{
+            color: gray;
+        }
+        `
+
+        shadow.appendChild(style)
     }
 }
 
-// definir elemento customizado (nome-da-tag, oQueVaiSerATag)
 customElements.define('card-news', CardNews)
