58e165b26c488a7999902451b0f634ede20ae39d
judah-lopes <lopes.judah@gmail.com>
2024-02-22 22:41:39 -0300

    doc: Setando Propriedades de classes
    Commit com:
    - tópico "Setando Propriedades de classes"

--- Mód. 4 - Fundamentos do Angular Framework/1 - Trabalhando com Web Components no Front-end/aula 03 - Criando componentes/src/Components/card-news.js
+++ Mód. 4 - Fundamentos do Angular Framework/1 - Trabalhando com Web Components no Front-end/aula 03 - Criando componentes/src/Components/card-news.js
@@ -1,13 +1,31 @@
-class Cardnews extends HTMLElement{
+class CardNews extends HTMLElement{
     constructor(){
         super()
 
         const shadow = this.attachShadow({ mode: "open" })
+        shadow.appendChild(this.build())
+        shadow.appendChild(this.styles())
 
-        build(){}
-
-        styles(){}
     }
+
+    build(){
+        const componentRoot = document.createElement("div")
+        componentRoot.setAttribute("class", "card")
+
+        const cardLeft = document.createElement("div")
+        cardLeft.setAttribute("class", "card-left")
+        
+        const cardRight = document.createElement("div")
+        cardRight.setAttribute("class", "card-right")
+
+        componentRoot.appendChild(cardLeft)
+        componentRoot.appendChild(cardRight)
+
+        return componentRoot
+    }
+    
+    styles(){}
+
 }
 
-customElements.define("card-news", Cardnews)
+customElements.define("card-news", CardNews)
