17a653a672f571cefe3d9d26f49b10ea72df35d9
judah-lopes <lopes.judah@gmail.com>
2024-02-22 22:27:55 -0300

    doc: Construindo um Component de maneira profissional
    Commit com:
    - tópico "Construindo um Component de maneira profissional"

--- Mód. 4 - Fundamentos do Angular Framework/1 - Trabalhando com Web Components no Front-end/aula 03 - Criando componentes/src/Components/card-news.js
+++ Mód. 4 - Fundamentos do Angular Framework/1 - Trabalhando com Web Components no Front-end/aula 03 - Criando componentes/src/Components/card-news.js
@@ -1,68 +1,13 @@
-class CardNews extends HTMLElement {
-    
+class Cardnews extends HTMLElement{
     constructor(){
         super()
 
         const shadow = this.attachShadow({ mode: "open" })
 
-        shadow.innerHTML = 
-        `
-            <div class="card">
-                <div class="card-left">
-                    <span>By Pedro Judah</span>
-                    <h1>Darth Vader Contrata Devs</h1>
-                    <p>Darth Vader procura desenvolvedores que saibam usar components</p>
-                </div>
-                <div class="card-right">
-                    <img src="assets/img/darth-vader.png" alt="darthinho" style="width: 300px;">
-                </div>
-            </div>
-        `
-        const style = document.createElement("style")
-        style.textContent = `
-        *{
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        
-            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-        }
-        
-        .card-news{
-            width: 80%;
-            
-            display: flex;
-            justify-content: space-between;
-        
-            box-shadow: 14px 10px 16px -7px rgba(0,0,0,0.55);
-            -webkit-box-shadow: 14px 10px 16px -7px rgba(0,0,0,0.55);
-            -moz-box-shadow: 14px 10px 16px -7px rgba(0,0,0,0.55);
-        }
-        
-        .card-left{
-            padding: 10px;
-        
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-        }
-        
-        .card-left span{
-            font-weight: 400;
-        }
-        
-        .card-left h1{
-            margin-top: 15px;
-            font-size: 25px;
-        }
-        
-        .card-left p{
-            color: gray;
-        }
-        `
+        build(){}
 
-        shadow.appendChild(style)
+        styles(){}
     }
 }
 
-customElements.define('card-news', CardNews)
+customElements.define("card-news", Cardnews)
