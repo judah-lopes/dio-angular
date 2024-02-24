f6ec0bc687545170c039533308f2936b850f4df1
judah-lopes <lopes.judah@gmail.com>
2024-02-24 16:58:14 -0300

    doc: Funções Multi Tipo
    Commit com:
    - tópico "Funções Multi Tipo"

--- Mód. 4 - Fundamentos do Angular Framework/2 - TypeScript para Angular/aula 02, 03 e 04/src/index.ts
+++ Mód. 4 - Fundamentos do Angular Framework/2 - TypeScript para Angular/aula 02, 03 e 04/src/index.ts
@@ -8,7 +8,12 @@ function addToHello(name:string) {
     return `Hello ${name}`
 }
 
+function callToPhone(phone:number | string): number | string /*: any*/ {
+    return phone
+}
+
 let soma: number = addNumber(4, 7)
 
 console.log(soma)
 console.log(addToHello("Judah"))
+console.log(callToPhone())
