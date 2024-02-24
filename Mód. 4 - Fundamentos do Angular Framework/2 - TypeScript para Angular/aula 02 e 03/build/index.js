1b1fd21aa4d0cb2d9dd0959407791e22a9dba084
judah-lopes <lopes.judah@gmail.com>
2024-02-24 15:36:19 -0300

    doc: Trabalhando com Arrays
    Commit com:
    - tópico "Trabalhando com Arrays"

--- Mód. 4 - Fundamentos do Angular Framework/2 - TypeScript para Angular/aula 02 e 03/build/index.js
+++ Mód. 4 - Fundamentos do Angular Framework/2 - TypeScript para Angular/aula 02 e 03/build/index.js
@@ -1,9 +1,23 @@
 "use strict";
-function printarObjeto(pessoa) {
-    console.log(pessoa);
-}
-printarObjeto({
-    nome: 'Bruce Wayne',
-    vulgo: 'Batman'
-});
-console.log('Olá mundo');
+//----- Variáveis -----
+//Tipos Primitivos: boolean, number, string
+let ligado = true;
+let telefone = 11;
+let nome = 'Judah';
+//Tipos Especiais: null, undefined
+let nulo = null;
+let indefinido = undefined;
+//Tipos Abrangentes: any, void
+let retorno;
+let retornoView = false;
+//Objeto - sem previsibilidade
+let produto = {
+    name: 'Judah',
+    cidade: 'AC',
+    idade: 30
+};
+let meuProduto = {
+    nome: 'Escova',
+    preco: 2.5,
+    unidades: 50
+};
--- Mód. 4 - Fundamentos do Angular Framework/2 - TypeScript para Angular/aula 02 e 03/src/index.ts
+++ Mód. 4 - Fundamentos do Angular Framework/2 - TypeScript para Angular/aula 02 e 03/src/index.ts
@@ -1,15 +1,44 @@
-type Hero = {
-    nome: string;
-    vulgo: string;
-};
+/*---------------- VARIÁVEIS ----------------*/
 
-function printarObjeto(pessoa:Hero) {
-    console.log(pessoa)
+    //Tipos Primitivos: boolean, number, string
+    let ligado: boolean = true
+    let telefone: number = 11
+    let nome: string = 'Judah'
+
+    //Tipos Especiais: null, undefined
+    let nulo: null = null
+    let indefinido: undefined = undefined
+
+    //Tipos Abrangentes: any, void
+    let retorno: void
+    let retornoView: any = false
+
+/*----------------- OBJETOS -----------------*/
+//Objeto - sem previsibilidade
+let produto: object = {
+    name: 'Judah',
+    cidade: 'AC',
+    idade: 30
 }
 
-printarObjeto({
-    nome: 'Bruce Wayne',
-    vulgo: 'Batman'
-})
+//Objeto Tipado - com previsibilidade
+type ProdutoLoja = {
+    nome: string,
+    preco: number,
+    unidades: number
+}
 
-console.log('Olá mundo')
+let meuProduto: ProdutoLoja = {
+    nome: 'Escova',
+    preco: 2.5,
+    unidades: 50
+}
+
+/*------------------ ARRAY ------------------*/
+let dados: string[] = ["Felipe", "Ana", "Adriana"]
+let dados2: Array<string> = ["Felipe", "Ana", "Adriana"]
+
+
+
+
+
