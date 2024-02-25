ed0aeb711059a5e2a928f3148927ce563678b39e
judah-lopes <lopes.judah@gmail.com>
2024-02-25 01:24:29 -0300

    doc: Atrribute Decorator
    Commit com:
    - tópico "Atrribute Decorator"

--- Mód. 4 - Fundamentos do Angular Framework/2 - TypeScript para Angular/aula 02-09/src/index.ts
+++ Mód. 4 - Fundamentos do Angular Framework/2 - TypeScript para Angular/aula 02-09/src/index.ts
@@ -1,5 +1,6 @@
 /*--------- DECORATORS ---------*/
 
+//class decorator
 function apiVersion(version: string) {
     return (target: any) => {
         Object.assign(target.prototype, { __version: version })
@@ -7,8 +8,35 @@ function apiVersion(version: string) {
     }
 }
 
-@apiVersion("1.10")
-class Api{}
+//attribute decorator
+function minlength(length: number){
+    return (target: any, key: string) => {
+        let _value = target[key]
 
-const api = new Api()
-console.log(api.__version)
+        const getter = () => `Você conseguiu, ${_value} tem mais de 2 letras`
+        const setter = (value: string) => {
+            if (value.length < length) {
+                throw new Error(`Tamanho menor do que ${length}`); 
+            }else{
+                _value = value
+            }
+        }
+
+        Object.defineProperty(target, key, {
+            get: getter,
+            set: setter
+        })
+    }
+}
+
+class Api{
+    @minlength(3)
+    name: string
+
+    constructor(name: string){
+        this.name = name
+    }
+}
+
+const api = new Api("Usuário")
+console.log(api.name)
